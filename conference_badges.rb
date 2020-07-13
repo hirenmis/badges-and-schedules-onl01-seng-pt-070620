04b3441d71c66b98017ab4543a5bafeec6fdcdef
Hiren Mistry <hirenmst@gmail.com>
2020-07-13 01:16:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+name = "Arel"
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator
+  def badge_maker(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  return badges
+end
+  
+
+  
