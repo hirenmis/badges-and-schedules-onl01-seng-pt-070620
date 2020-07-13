af9e79fd5fc1f003dd1323e8f428f5e983ab3211
Hiren Mistry <hirenmst@gmail.com>
2020-07-13 01:17:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,10 @@
-name = "Arel"
+# name = "Arel"
 
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
+# def badge_maker(name)
+#   "Hello, my name is #{name}."
+# end
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator
   def badge_maker(attendees)
